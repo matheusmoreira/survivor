ee084ab84e79c89f94c9bad65d220e441f202ca4
Matheus Moreira <matheus.a.m.moreira@gmail.com>
2011-11-11 18:06:59 -0200

    Add Creature class and have Character subclass it.
    Add general move method for creatures, and let the caller specify to the specific direction methods how many tiles he would like to move.

--- lib/survivor/game/character.rb
+++ lib/survivor/game/character.rb
@@ -1,52 +1,8 @@
-require 'survivor/game/map/coordinates'
+require 'survivor/game/creature'
 
 module Survivor
   class Game
-    class Character
-
-      attr_accessor :coordinates
-
-      def initialize game
-        @game = game
-        self.coordinates = Survivor::Game::Map::Coordinates.new
-      end
-
-      def x= value
-        self.coordinates = Survivor::Game::Map::Coordinates.new value, y
-      end
-
-      def y= value
-        self.coordinates = Survivor::Game::Map::Coordinates.new x, value
-      end
-
-      def x
-        coordinates.x
-      end
-
-      def y
-        coordinates.y
-      end
-
-      def move_up
-        self.y += 1
-      end
-
-      def move_down
-        self.y -= 1
-      end
-
-      def move_left
-        self.x -= 1
-      end
-
-      def move_right
-        self.x += 1
-      end
-
-      def surroundings
-        @game.map.area_around self
-      end
-
+    class Character < Creature
     end
   end
 end
---
+++ lib/survivor/game/creature.rb
@@ -0,0 +1,59 @@
+require 'survivor/game/map/coordinates'
+
+module Survivor
+  class Game
+    class Creature
+
+      attr_accessor :coordinates
+
+      def initialize game
+        @game = game
+        self.coordinates = Survivor::Game::Map::Coordinates.new
+      end
+
+      def x= value
+        self.coordinates = Survivor::Game::Map::Coordinates.new value, y
+      end
+
+      def y= value
+        self.coordinates = Survivor::Game::Map::Coordinates.new x, value
+      end
+
+      def x
+        coordinates.x
+      end
+
+      def y
+        coordinates.y
+      end
+
+      def move dx, dy
+        tap do |c|
+          c.x += dx
+          c.y += dy
+        end
+      end
+
+      def move_up n = 1
+        move 0, n
+      end
+
+      def move_down n = 1
+        move 0, -n
+      end
+
+      def move_left n = 1
+        move -n, 0
+      end
+
+      def move_right n = 1
+        move n, 0
+      end
+
+      def surroundings
+        @game.map.area_around self
+      end
+
+    end
+  end
+end
