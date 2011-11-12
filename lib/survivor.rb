3035e0f04930b8960c66ddc6fa0666a95b5e939f
Matheus Moreira <matheus.a.m.moreira@gmail.com>
2011-11-12 19:55:27 -0200

    Adapt Game#run to take an empty option hash by default.

--- lib/survivor.rb
+++ lib/survivor.rb
@@ -2,6 +2,10 @@ require 'survivor/game'
 
 module Survivor
 
+  def self.run options = {}
+    Survivor::Game.new.run
+  end
+
   def self.root
     File.expand_path '../..', __FILE__
   end
@@ -12,10 +16,6 @@ module Survivor
 
   def self.map filename
     File.join maps, filename
-  end
-
-  def self.run(argv)
-    Survivor::Game.new.run
   end
 
 end
