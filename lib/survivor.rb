b8567656c301b7ed0008d3f890772a3b6ca0f085
Matheus Moreira <matheus.a.m.moreira@gmail.com>
2011-11-12 02:12:31 -0200

    Add path helpers.

--- lib/survivor.rb
+++ lib/survivor.rb
@@ -2,6 +2,18 @@ require 'survivor/game'
 
 module Survivor
 
+  def self.root
+    File.expand_path '../..', __FILE__
+  end
+
+  def self.maps
+    File.join root, 'maps'
+  end
+
+  def self.map filename
+    File.join maps, filename
+  end
+
   def self.run(argv)
     Survivor::Game.new.run
   end
