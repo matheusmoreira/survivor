fe008008481b46b238188e54bae6758713293ffb
Matheus Moreira <matheus.a.m.moreira@gmail.com>
2011-11-12 02:34:01 -0200

    Add Map::CustomYamlFormat class that handles YAML serialization in a custom, easily editable format.

---
+++ lib/survivor/game/map/custom_yaml_format.rb
@@ -0,0 +1,86 @@
+require 'survivor/game/map'
+require 'survivor/game/map/coordinates'
+require 'survivor/game/map/tile'
+require 'survivor/version'
+require 'yaml'
+
+module Survivor
+  class Game
+    class Map
+      class CustomYamlFormat
+
+        def self.save map, filename
+          File.open(filename, 'w') do |file|
+            file.write custom_structure_for(map).to_yaml
+          end
+        end
+
+        def self.from_file filename
+          from_yaml File.open(filename) { |file| YAML::load file }
+        end
+
+        def self.from_yaml string
+          survivor = string['survivor']
+          version = survivor['version']
+          unless Survivor::Version == version
+            puts "Map meant for Survivor v#{version} - current version is v#{Survivor::Version}"
+          end
+          build_map survivor['map']
+        end
+
+        private
+
+        def self.build_map map_hash
+          tiles = map_hash['tiles']
+          Map.new.tap do |map|
+            map_hash['data'].split("\n").tap do |lines|
+              lines.each_with_index do |line, y|
+                line.chars.each_with_index do |char, x|
+                  if tile_data = tiles[char]
+                    passable = tile_data['passable']
+                  end
+                  tile = Tile.new char, passable
+                  map[Coordinates[x, lines.count - y - 1]] = tile
+                end
+              end
+            end
+            start = map_hash['starting point']
+            x, y = start['x'].to_i, start['y'].to_i
+            puts "(#{x}, #{y})"
+            map.starting_point = Coordinates[x, y]
+          end
+        end
+
+        def self.custom_structure_for map
+          {}.tap do |data|
+            (data['survivor'] = {}).tap do |survivor|
+              survivor['version'] = Survivor::Version.to_s
+              (survivor['map'] = {}).tap do |map_hash|
+                (map_hash['starting point'] = {}).tap do |starting_point|
+                  starting_point['x'] = map.starting_point.x
+                  starting_point['y'] = map.starting_point.y
+                end
+                (map_hash['tiles'] = {}).tap do |tiles|
+                  map.tiles.uniq.each do |tile|
+                    (tiles[tile.char] = {}).tap do |tile_data|
+                      tile_data['passable'] = tile.passable?
+                    end
+                  end
+                end
+                (map_hash['data'] = '').tap do |data|
+                  map.each_line do |line|
+                    line.each do |tile|
+                      data << tile.char
+                    end
+                    data << "\n"
+                  end
+                end
+              end
+            end
+          end
+        end
+
+      end
+    end
+  end
+end
