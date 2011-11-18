6035d11f12d8581cbe488620a4a872f837e7cf0b
Matheus Moreira <matheus.a.m.moreira@gmail.com>
2011-11-17 23:44:53 -0200

    Don't define option query methods multiple times.

--- lib/survivor/options.rb
+++ lib/survivor/options.rb
@@ -5,14 +5,8 @@ module Survivor
       @options = options
     end
 
-    BOOLEAN_OPTIONS = [ :development_mode ].freeze
-
-    { :enable => true, :disable => false }.each_pair do |set, state|
-      BOOLEAN_OPTIONS.each do |boolean_option|
-
-        define_method("#{set}_#{boolean_option}".to_sym) do
-          @options[boolean_option] = state
-        end
+    BOOLEAN_OPTIONS = [ :development_mode ].freeze.tap do |boolean_options|
+      boolean_options.each do |boolean_option|
 
         define_method("#{boolean_option}_enabled?".to_sym) do
           @options[boolean_option]
@@ -20,6 +14,12 @@ module Survivor
 
         define_method("#{boolean_option}_disabled?".to_sym) do
           not @options[boolean_option]
+        end
+
+        { :enable => true, :disable => false }.each_pair do |set, state|
+          define_method("#{set}_#{boolean_option}".to_sym) do
+            @options[boolean_option] = state
+          end
         end
 
       end
