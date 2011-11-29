60f90d1a8ee7bae5801053589c40e0478eb8b376
Matheus Moreira <matheus.a.m.moreira@gmail.com>
2011-11-29 12:50:06 -0200

    Add zombie creature.

---
+++ lib/survivor/game/creature/zombie.rb
@@ -0,0 +1,15 @@
+require 'survivor/game/creature'
+
+module Survivor
+  class Game
+    class Creature
+      class Zombie < Creature
+
+        def initialize
+          super 'Z'
+        end
+
+      end
+    end
+  end
+end
